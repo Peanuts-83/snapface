dfff5db719d258de0002b69f99a7ce26ee1e068e
Peanuts-83 <tranque@free.fr>
2022-11-12 15:42:25 +0100

    1st

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent {
+
+  constructor() { }
+
+
+}
